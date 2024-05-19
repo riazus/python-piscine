178f5a9b98f5867963d6c2d13300bfeafae3f2d8
riazus <riyaz@alexlegal.fr>
2024-05-19 15:16:08 +0200

    start module-01 ex00

---
+++ module-01/ex00/give_bmi.py
@@ -0,0 +1,35 @@
+def give_bmi(height: list[int | float],
+             weight: list[int | float]) -> list[int | float]:
+    """
+    Function for calculataing BMI.
+    Takes two arrays and makes following calculations:
+    weight / height ** 2
+    """
+    if len(height) != len(weight):
+        raise AssertionError("args are invalid")
+
+    res = list()
+    i = 0
+    while i < len(height):
+        validWeight = isinstance(weight[i], (int, float))
+        validHeight = isinstance(height[i], (int, float))
+
+        if not validWeight or not validHeight:
+            raise AssertionError("args are invalid")
+        res.append(weight[i] / (height[i] ** 2))
+        i += 1
+
+    return res
+
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """
+    Checks each BMI for limitation.
+    True if the value is above limit
+    """
+    res = list()
+
+    for b in bmi:
+        res.append(b > limit)
+
+    return res
